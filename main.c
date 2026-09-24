d72dc4aea7b8648e661c6d39974c60c76eb58395
samad-source <s.azeez8553@miva.edu.ng>
2026-09-24 10:17:04 +0100

    C basics

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 int main(){
     printf("Hello, World!\n");
     int age = 25;
-    printf("Age: %d", age);
+    printf("Age: %d", age );
+    /*scanf("%d", &age);*/
+    printf("Updated Age: %d", age);
     return 0;
 }
